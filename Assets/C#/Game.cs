a91f755cf4d8e90434f4e6e9fa3e067ac1bccbc8
“YangHongQiang-Y” <13061250265@163.com>
2021-01-14 12:25:32 +0800

    first commit

---
+++ Assets/C#/Game.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Game : MonoBehaviour {
+
+    public Transform start;
+    public Transform really;
+    public int score = 0;
+    public GameObject pinPerfab;
+    public pin nowpin;
+    public bool isGameOver = false;
+    public Text scoretext;
+    public Camera mainCamera;
+    public float speed = 3;
+	// Use this for initialization
+	void Start () {
+        start = GameObject.Find("start").transform;
+        really = GameObject.Find("really").transform;
+        mainCamera = Camera.main;
+        setpin();
+	}
+
+    private void Update()
+    {
+        if (isGameOver)
+            return;
+        if(Input.GetMouseButtonDown(0))
+        {
+            score++;
+            scoretext.text = score.ToString();
+            nowpin.StartFly();
+            setpin();
+        }
+    }
+
+    // Update is called once per frame
+    void setpin () {
+        nowpin = GameObject.Instantiate(pinPerfab, really.position, pinPerfab.transform.rotation).GetComponent<pin>();
+		
+	}
+    public void GameOver()
+    {
+        if (isGameOver)
+            return;
+        GameObject.Find("Circle").GetComponent<xuanzhuan>().enabled = false;
+        StartCoroutine(GameOverAnimaation());
+        isGameOver = true;
+    }
+     IEnumerator GameOverAnimaation()
+    {
+        while(true)
+        {
+            mainCamera.backgroundColor = Color.Lerp(mainCamera.backgroundColor, Color.red, speed * Time.deltaTime);
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 4, speed * Time.deltaTime);
+            if(Mathf.Abs(mainCamera.orthographicSize-4)<=0.01f)
+            {
+                break;
+            }
+            yield return 0;
+        }
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/C#/pin.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pin : MonoBehaviour {
+
+    public float speed = 15; 
+    public bool isfly = false;
+    public bool isreach = false;
+    public Transform start;
+    public Transform Circle;
+    public Vector3 finallypostion;
+	// Use this for initialization
+	void Start () {
+        start = GameObject.Find("start").transform;
+        Circle = GameObject.Find("Circle").transform;
+        finallypostion = Circle.position;
+        finallypostion.y = finallypostion.y - 2f;
+	}
+
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(isfly==false)
+        {
+            if(isreach==false)
+            {
+                transform.position=Vector3.MoveTowards(transform.position, start.position, speed * Time.deltaTime);
+                if(Vector3.Distance(transform.position,start.position)<=0.05f)
+                {
+                    isreach = true;
+                }
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, finallypostion, speed * Time.deltaTime);
+            if(Vector3.Distance(transform.position,finallypostion)<0.05f)
+            {
+                transform.position = finallypostion;
+                transform.parent = Circle;
+                isfly = false;
+            }
+        }
+	}
+    public void StartFly()
+    {
+        isfly = true;
+        isreach = true;
+    }
+
+
+
+}
